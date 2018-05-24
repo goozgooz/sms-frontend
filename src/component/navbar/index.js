7b3b2380ec7064abcceb468ce15fe69be426f38a
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-05-24 19:55:15 +0000

    adding hamburger

--- src/component/navbar/index.js
+++ src/component/navbar/index.js
@@ -1,14 +1,22 @@
 import './_navbar.scss';
 import React from 'react';
 
-// home | inventory | service depart | about us | contact us
+import fontawesome from '@fortawesome/fontawesome';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import {faBars} from '@fortawesome/fontawesome-free-solid';
 
+fontawesome.config = { autoAddCss: false };
+
+
+// home | inventory | service depart | about us | contact us
+      
 
 class MyNavbar extends React.Component {
   render(){
     return(
       <nav className='navbar'>
-        <ul>
+        <button id='hamburger'> <FontAwesomeIcon icon={faBars} size='xs' /> </button>   
+        <ul id='nav-menu'>
           <li> <a href='#'> Home </a> </li>
           <li> <a href='#'> Inventory </a> </li>
           <li> <a href='#'> Service </a> </li>
