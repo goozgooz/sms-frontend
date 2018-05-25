57bb7d8d718e315df51af0b3188152f783f017fb
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-05-25 15:54:22 +0000

    hamburger is clickable, styling next

--- src/component/navbar/index.js
+++ src/component/navbar/index.js
@@ -1,5 +1,6 @@
 import './_navbar.scss';
 import React from 'react';
+import * as _ from '../../lib/util.js';
 
 import fontawesome from '@fortawesome/fontawesome';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
@@ -12,11 +13,39 @@ fontawesome.config = { autoAddCss: false };
       
 
 class MyNavbar extends React.Component {
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      showMobileNav: false,
+    };
+      
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+  
+  toggleMenu(e){
+    e.preventDefault();
+    this.setState({showMobileNav:!this.state.showMobileNav});
+  }
+  
   render(){
+    console.log(this.state);
     return(
       <nav className='navbar'>
-        <button id='hamburger'> <FontAwesomeIcon icon={faBars} size='xs' /> </button>   
-        <ul id='nav-menu'>
+        <button id='hamburger' onClick={this.toggleMenu}> 
+          <FontAwesomeIcon icon={faBars} size='xs' /> 
+        </button>
+        {_.renderIf(this.state.showMobileNav,
+          <ul id='hamburger-nav'>
+            <li> <a href='#'> Home </a> </li>
+            <li> <a href='#'> Inventory </a> </li>
+            <li> <a href='#'> Service </a> </li>
+            <li> <a href='#'> About </a> </li>
+            <li> <a href='#'> Contact </a> </li>
+          </ul>
+        )}
+        
+        <ul id='regular-nav'>
           <li> <a href='#'> Home </a> </li>
           <li> <a href='#'> Inventory </a> </li>
           <li> <a href='#'> Service </a> </li>
