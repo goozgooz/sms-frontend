0c00d98335a0f3a4aafaf8b195fcd7e0062c5250
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-06-19 21:00:55 -0700

    rework

--- src/component/banner/index.js
+++ src/component/banner/index.js
@@ -5,7 +5,9 @@ import React from 'react';
 class Banner extends React.Component {
   render(){
     return(
-      <img id='banner' src={require('./assets/dealer.jpg')} />
+      <div className='banner'>
+        <img src={require('./assets/dealer.jpg')} />
+      </div>
     );
   }
 }
