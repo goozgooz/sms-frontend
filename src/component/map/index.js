1a06ce243347e4d2ebd412f62a70114d4032bd14
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-07-02 19:53:32 +0000

    implementing map

---
+++ src/component/map/index.js
@@ -0,0 +1,6 @@
+import {GoogleApiWrapper} from 'google-maps-react';
+
+     defaultCenter={{lat:47.6270856, lng:-122.1621511}}
+     
+     
+     // AIzaSyCHsCYLeGX6VmvyGSvRZk7Syo1VWg5Gf2U
--- src/component/tiles/index.js
+++ src/component/tiles/index.js
@@ -3,24 +3,26 @@ import './_tiles.scss';
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+import Map from '../map';
+
 class Tiles extends React.Component {
   render(){
     return (
       <div className='tiles'>
         <div>
           <Link to='/inventory'> View Inventory </Link>
+          
+          <Map />
         </div>
         
-        <div>
-          <Link to='/'> Vehicle Finder </Link>
-        </div>
-        
-        <div>
-          <Link to='/contact'> Contact Us </Link>
-        </div>
+
+       
+
       </div>
     );
   }
 };
 
 export default Tiles;
+
+
