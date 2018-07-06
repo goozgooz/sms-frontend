68a257cf216f1929928e0997a3b7aa7443d179e7
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-07-05 17:59:06 -0700

    header formatted with links

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -11,15 +11,19 @@ class Header extends React.Component{
     return (
       <div className='header'>
         <h1> SOURCE MOTOR SALES </h1>
-        <a 
-          href='https://www.google.com/maps/dir//Source+Motor+Sales,+13407+NE+20th+St+%233,+Bellevue,+WA+98005/'
-          target='_blank'
-        >
+
+      
+        <a href='https://www.google.com/maps/dir//Source+Motor+Sales,+13407+NE+20th+St+%233,+Bellevue,+WA+98005/'target='_blank'>
           <FontAwesomeIcon icon={faMapMarkerAlt} className='icon'/>
-          <h2> <FontAwesomeIcon icon={faPhone} className='icon' />2420 NE 21st Belleve, WA 98005 </h2> 
-        </a>  
+          <h2> 2420 NE 21st Belleve, WA 98005 </h2>
+        </a>
+
+      <div className='call'>
+        <FontAwesomeIcon icon={faPhone} className='icon' />
         <h2> 425-555-5555 </h2> 
       </div>
+
+    </div>
     );
   }
 }
