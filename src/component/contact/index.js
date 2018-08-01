256587a886182f9e6ecdd37c4a65bc3b39958f30
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-08-01 20:04:05 +0000

    contact page info in

--- src/component/contact/index.js
+++ src/component/contact/index.js
@@ -1,12 +1,47 @@
 import './_contact.scss';
 import React from 'react';
 
-import Construction from '../construction';
+import Navbar from '../navbar';
+import Footer from '../footer';
+import Map from '../map';
 
 class Contact extends React.Component {
   render(){
     return (
-      <Construction />
+      <div className='contact-page'>
+        <Navbar />
+        
+        <div className='layout'>
+          
+          <h1> Contact Me! </h1>
+          
+          <ul>
+            <li> Buying a car? </li>
+            <li> Selling a car? </li>
+            <li> Want help finding a car? </li>
+            <li> Need some work done to your car? </li>
+            <li> Don't hesitate to reach out to me! </li>
+          </ul>
+          
+          <div className='contact-info'> 
+            <h3> Contact Information </h3>
+            <h3> Phone: </h3> 
+            <h3> Email: </h3>
+          </div>
+          
+          <div className='office-info'>
+            <h3> Business Hours: </h3>
+            <div className='map-container'>
+              <Map />
+            </div>
+          </div>
+        
+      
+        
+        </div>
+        
+        <Footer />
+      </div>
     );
   }
 }
