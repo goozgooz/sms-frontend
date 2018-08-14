3fd0e80902b775bbf9ae71ea0375691d45cb1cc4
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-08-13 19:39:41 -0700

    desktop contact styling

--- src/component/contact/index.js
+++ src/component/contact/index.js
@@ -22,34 +22,37 @@ class Contact extends React.Component {
             <li className='emphasis'> Don't hesitate to reach out to me! </li>
           </ul>
           
-          <div className='contact-section'> 
-            <div className='section-header'>
-              <h4> Contact Information: </h4>
-              {/* <span className='line' /> */}
+          <div className='contact-info'>
+            <div className='contact-section'> 
+              <div className='section-header'>
+                <h4> Contact Information: </h4>
+                {/* <span className='line' /> */}
+              </div>
+
+              <h5> Reza Jaberi </h5>
+
+              <a href="tel: +14255625131">
+                <h5> Phone: 425-562-5131 </h5>
+              </a>        
+
+              <h5> Email: sourcemotors@gmail.com </h5>
             </div>
+            
+            <div className='contact-section'>
+              <div className='section-header'>
+                <h4> Business Hours: </h4>
+                {/* <span className='line' /> */}
+              </div>
 
-            <h5> Reza Jaberi </h5>
-
-            <a href="tel: +14255625131">
-              <h5> Phone: 425-562-5131 </h5>
-            </a>        
-
-            <h5> Email: sourcemotors@gmail.com </h5>
+              <h5 className='emphasis'> Monday-Friday: </h5> <h5> 8am-5pm </h5>
+              <h5> Saturday: By Appointment Only </h5>
+            </div>
           </div>
-          
-          <div className='contact-section'>
-            <div className='section-header'>
-              <h4> Business Hours: </h4>
-              {/* <span className='line' /> */}
-            </div>
 
-            <h5> Monday-Friday: 8am-5pm </h5>
-            <h5> Saturday: By Appointment Only </h5>
-            <div className='map-container'>
-              <Map />
-            </div>
+          <div className='map-container'>
+            <Map />
           </div>
-        
+
         <Footer />
 
         </div>
