4d9ab81f14ebeae11b273ac641795136040f7124
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-08-23 17:29:09 -0700

    navbar icons implemented

--- src/component/navbar/mobile.js
+++ src/component/navbar/mobile.js
@@ -1,9 +1,12 @@
 import './_mobile.scss';
 import React from 'react';
-
 import {Link} from 'react-router-dom';
 
 import {fallDown as Menu} from 'react-burger-menu';
+import fontawesome from '@fortawesome/fontawesome';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import {faMapMarkerAlt, faPhone} from '@fortawesome/fontawesome-free-solid';
+fontawesome.config = { autoAddCss: false };
 
 class MobileNav extends React.Component {
   showSettings(e){
@@ -17,7 +20,17 @@ class MobileNav extends React.Component {
   render(){
     return(
       <div className='navbar'>
-        <div className='derp'> </div>
+        <div className='logo'>
+          <Link className='menu-item' to='/'> SMS </Link>
+        </div>
+        <div className='contact-icon-holder'> 
+          <a href='tel:+14255625131'> 
+            <FontAwesomeIcon icon={faPhone} className='icon' />
+          </a>
+          <a href='https://www.google.com/maps/dir//Source+Motor+Sales,+13407+NE+20th+St+%233,+Bellevue,+WA+98005/'target='_blank'>
+            <FontAwesomeIcon icon={faMapMarkerAlt} className='icon'/> 
+          </a>
+        </div>
         <Menu width={'100%'}>
           <Link className='menu-item' to='/'> Home </Link>
           <Link className='menu-item' to='/inventory'> Inventory </Link> 
