3a701e4b5efd3e295bbb7e60abad661c27e67676
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-08-31 15:58:31 -0700

    mobile map redone

--- src/component/map/index.js
+++ src/component/map/index.js
@@ -15,24 +15,24 @@ class Map extends React.Component{
         <div className='location-info'>
           <h3> Shop Hours </h3>
           <div className='shop-hours'>
-            <h3> Monday-Friday: </h3> <h3> 8AM - 5PM </h3>
+            <h4> Monday-Friday: </h4> <h4> 8AM - 5PM </h4>
           </div>
           <div className='shop-hours'>
-            <h3> Saturday: </h3> <h3> By Appointment Only </h3>
+            <h4> Saturday: </h4> <h4> By Appointment Only </h4>
           </div>
           
         
           <a href='https://www.google.com/maps/dir//Source+Motor+Sales,+13407+NE+20th+St+%233,+Bellevue,+WA+98005/'target='_blank'>
             <FontAwesomeIcon icon={faMapMarkerAlt} className='icon'/> 
             <div className='location'>
-              <h3> 13407 NE 20th St #3 </h3>
-              <h3> Bellevue, WA 98005 </h3>
+              <h4> 13407 NE 20th St #3 </h4>
+              <h4> Bellevue, WA 98005 </h4>
             </div>
           </a>
 
           <a href='https://www.google.com/maps/dir//Source+Motor+Sales,+13407+NE+20th+St+%233,+Bellevue,+WA+98005/'target='_blank'>
             <FontAwesomeIcon icon={faPhone} className='icon'/> 
-            <h3> 425-562-5131 </h3>
+            <h4> 425-562-5131 </h4>
           </a>
           
           
