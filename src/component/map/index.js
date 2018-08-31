e02e8565f670ebd2bcc4ff72bccb679398207665
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-08-31 16:29:06 +0000

    new map stuff added - need to style on mbp

--- src/component/map/index.js
+++ src/component/map/index.js
@@ -13,6 +13,15 @@ class Map extends React.Component{
     return(
       <div className='map'>
         <div className='location-info'>
+          <h3> Shop Hours </h3>
+          <div className='shop-hours'>
+            <h3> Monday-Friday: </h3> <h3> 8AM - 5PM </h3>
+          </div>
+          <div className='shop-hours'>
+            <h3> Saturday: </h3> <h3> By Appointment Only </h3>
+          </div>
+          
+        
           <a href='https://www.google.com/maps/dir//Source+Motor+Sales,+13407+NE+20th+St+%233,+Bellevue,+WA+98005/'target='_blank'>
             <FontAwesomeIcon icon={faMapMarkerAlt} className='icon'/> 
             <div className='location'>
@@ -25,6 +34,8 @@ class Map extends React.Component{
             <FontAwesomeIcon icon={faPhone} className='icon'/> 
             <h3> 425-562-5131 </h3>
           </a>
+          
+          
         </div>     
 
         <div className='map-container'>
