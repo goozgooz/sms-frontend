f0502717d16115eaeccdc519447f42f1d293fc8a
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-09-25 14:18:21 +0000

    about me blurb put on home page

--- src/component/welcome/index.js
+++ src/component/welcome/index.js
@@ -8,9 +8,9 @@ class Welcome extends React.Component {
       <div className='welcome'>
         <h3> Welcome to </h3>
         <h3> Source Motor Sales & Service </h3>
-        <p className='welcome-message'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p className='welcome-message'> At Source Motors we have no salesmen. I am the owner and sole individual responsible for helping you buy and sell your premium used car. The majority of my business is now based off of referrals and repeat customers, and as a result I keep a very small inventory, but I am here to help you track down whatever you may need. If you're not sure what car is right for you, give me a call and we can chat and work together to figure out what is best for you. Source Motors also has a full service department. If your car needs any repairs or routine maintenance, give us a call and we can give you a no frills, dealership alternative for taking care of your car. </p>
       </div>
-    )
+    );
   }
 };
 
