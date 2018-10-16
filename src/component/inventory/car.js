12e6fbfa505629c4ba91ba07a17ac47796121373
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-10-16 16:57:56 +0000

    added photo carousel

--- src/component/inventory/car.js
+++ src/component/inventory/car.js
@@ -15,39 +15,29 @@ class Car extends React.Component {
   
   render(){
     let {car, displayFull, back} = this.props;
-    console.log(displayFull);
  
     return(
       <React.Fragment>
         {_.renderIf(displayFull,
-          <React.Fragment>
-            <div onClick={back} className='back-button'> 
-              <FontAwesomeIcon icon={faArrowLeft} className='icon' /> 
-            </div>
-            
-            <h3>  {car.year} {car.make} {car.model} ${car.price} </h3>
-            <img src={require('./dev-car.jpg')} />
-            <p> {car.headline} </p>
-            <hr />
-            <p> {car.exteriorColor} Exterior - {car.interiorColor} Interior </p>
-            <p> {car.miles} Miles - {car.transmission} Transmission </p>
-            <p> VIN: {car.vin} </p>
-          </React.Fragment>
-          
-          ,
-          
-          <React.Fragment>
-            <h3>  {car.year} {car.make} {car.model} ${car.price} </h3>
-            <img src={require('./dev-car.jpg')} />
-            <p> {car.headline} </p>
-            <hr />
-            <p> {car.exteriorColor} Exterior - {car.interiorColor} Interior </p>
-            <p> {car.miles} Miles - {car.transmission} Transmission </p>
-            <p> VIN: {car.vin} </p>
-          </React.Fragment>
-          
+          <div onClick={back} className='back-button'> 
+            <FontAwesomeIcon icon={faArrowLeft} className='icon' /> 
+          </div>
         )}
-
+            
+        <h3>  {car.year} {car.make} {car.model} ${car.price} </h3>
+        <img src={require('./dev-car.jpg')} />
+        
+        <div className='car-description'>
+          <p> {car.headline} </p>
+          <p> <span>Price: </span> ${car.price} </p>
+          <p> <span>Miles: </span> {car.miles} </p>
+          <p> <span>Exterior: </span> {car.exteriorColor} </p>
+          <p> <span>Interior: </span> {car.interiorColor} </p>
+          <p> <span>Transmission: </span> {car.transmission} </p>
+          <p> <span>Drive: </span> {car.drive} </p>
+          <p> <span>Title: </span> {car.title} </p>
+          <p> <span>VIN: </span> {car.vin} </p>
+        </div>
         
       </React.Fragment>
     );
