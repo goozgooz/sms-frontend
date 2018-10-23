7c35c8750b8bf86ac38cf41715ed40c905efa21b
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-10-23 19:51:40 +0000

    working carousel image function

--- src/component/inventory/car-photo.js
+++ src/component/inventory/car-photo.js
@@ -5,11 +5,25 @@ import Slider from 'react-slick';
 class CarPhotos extends React.Component{
   constructor(props){
     super(props);
+    
+    this.imageCarousel = this.imageCarousel.bind(this);
+  }
+  
+  imageCarousel(images) {
+    images.map((url, i) => {
+      if(i === 0) {
+        return (
+          <div>
+            <img src={url.main} />
+          </div>
+        );
+      }
+    });
   }
   
   render(){
     let {images} = this.props.folder;
-    console.log(images);
+    
     const settings = {
       dots: false,
       infinite: true,
@@ -17,17 +31,10 @@ class CarPhotos extends React.Component{
       slidesToShow: 1,
       slidesToScroll: 1,
     };
-        // <div>
-        //   <img src={require('./dev-car.jpg')} />
-        // </div>
-    
+
     return(
       <Slider {...settings} className='photo-carousel'>
-        {images.map((url, i) => {(
-          <div>
-            <a href='https://dl.dropboxusercontent.com/apitl/1/AAA9TUrWalD_A9juCjhCziFUf317AlBb_BnjXeQpyTlIDsIRB8nnAmsiH-XeAyAvHd4hZ3B3-yTNnYxrzWV30zKTmAVL97MdeiEIK4d8xWz1QvqIPWtqPSpeh32eC2d7lBFQBV8lCNdPvl_YYKRRypvKA7U9QkSdaQR3lA4mXRJFsMj49KoKatXl_MDhgLX4WwFX5jjeUlAgoUy9WS53Q3lec5z0u2F2lM-plwYoUwxO6hSsAOGEme3rpWgdasmaFiAHnuS8OYSYdTpAXmKE8gxmcVE0LX74l0SZdq9OKaMAKFRBuNJ19fxeBnXRI14QMmymzpcrdyFXkLA-mTfBg_DaRfn67mNnFQq1NBOit4RWhA' />
-          </div>
-        )})}
+        {this.imageCarousel(images)}
       </Slider>
     );
   }
