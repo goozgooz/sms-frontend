51071f31d7c03e97a925995fef5bd68722ed38cb
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-11-07 15:49:48 +0000

    few more done

--- src/component/tiles/index.js
+++ src/component/tiles/index.js
@@ -8,36 +8,34 @@ import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import {faCar, faWrench, faPhone} from '@fortawesome/fontawesome-free-solid';
 fontawesome.config = { autoAddCss: false };
 
-class Tiles extends React.Component {
-  render(){
-    return (
-      <div className='tiles'>
-      
-      
-        <div className='tile-item'>
-          <h3> View Inventory </h3>
-          <Link to='/inventory'>  
-            <FontAwesomeIcon icon={faCar} className='icon' />
-          </Link>
-        </div>
-
-        <div className='tile-item'>
-          <h3> Service Department </h3>
-          <Link to='/service'>  
-            <FontAwesomeIcon icon={faWrench} className='icon' />
-          </Link>
-        </div>
-
-        <div className='tile-item'>
-          <h3> Contact Me </h3>
-          <Link to='/contact'>  
-            <FontAwesomeIcon icon={faPhone} className='icon' />
-          </Link>
-        </div>
-        
+const Tiles = () => {
+  return (
+    <div className='tiles'>
+    
+    
+      <div className='tile-item'>
+        <h3> View Inventory </h3>
+        <Link to='/inventory'>  
+          <FontAwesomeIcon icon={faCar} className='icon' />
+        </Link>
       </div>
-    );
-  }
+
+      <div className='tile-item'>
+        <h3> Service Department </h3>
+        <Link to='/service'>  
+          <FontAwesomeIcon icon={faWrench} className='icon' />
+        </Link>
+      </div>
+
+      <div className='tile-item'>
+        <h3> Contact Me </h3>
+        <Link to='/contact'>  
+          <FontAwesomeIcon icon={faPhone} className='icon' />
+        </Link>
+      </div>
+      
+    </div>
+  );
 };
 
 export default Tiles;
