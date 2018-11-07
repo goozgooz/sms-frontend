f1e899fa2aedc61f3d5945bdeaa3e1096fb19764
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-11-07 15:21:53 +0000

    navbar is now stateless

--- src/component/navbar/index.js
+++ src/component/navbar/index.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import Mobile from './mobile.js';
 import Desktop from './desktop.js';
-
 import MediaQuery from 'react-responsive';
 
-class MyNavbar extends React.Component {
+// class MyNavbar extends React.Component {
 
-  render(){
-    return(
-      <React.Fragment>
-        <MediaQuery minDeviceWidth={768}>
-          <Desktop />
-        </MediaQuery>
+//   render(){
+//     return(
+//       <React.Fragment>
+//         <MediaQuery minDeviceWidth={768}>
+//           <Desktop />
+//         </MediaQuery>
         
-        <MediaQuery maxDeviceWidth={768}>
-          <Mobile />
-        </MediaQuery>
+//         <MediaQuery maxDeviceWidth={768}>
+//           <Mobile />
+//         </MediaQuery>
         
-      </React.Fragment>
-    );
-  }
-}
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+const MyNavbar = () => {
+  return (
+    <React.Fragment>
+      <MediaQuery minDeviceWidth={768}>
+        <Desktop />
+      </MediaQuery>
+        
+      <MediaQuery maxDeviceWidth={768}>
+        <Mobile />
+      </MediaQuery>
+      
+    </React.Fragment>
+  );
+};
 
 export default MyNavbar;
 
