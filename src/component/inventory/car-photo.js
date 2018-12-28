44fa0fb8da3b6fa98fa58016d83bdf6834067f3c
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-12-28 16:32:36 +0000

    car-photo now stateless

--- src/component/inventory/car-photo.js
+++ src/component/inventory/car-photo.js
@@ -2,42 +2,35 @@ import './_car.scss';
 import React from 'react';
 import Slider from 'react-slick';
 
-class CarPhotos extends React.Component{
-  constructor(props){
-    super(props);
-    
-  }
+const CarPhotos = (props) => {
+  let {displayAll, sold} = props;
+  let {images} = props.folder;
+
+  const carPhotos = images.map((url,i) => {
+    if(i === 0) {
+      return <div key={i}> <img src={url.main} /> </div>
+    }
+    if(i > 0) {
+      return <div key={i}> <img src={url.images} /> </div>
+    }
+  });
   
-  render(){
-    let {displayAll, sold} = this.props;
-    let {images} = this.props.folder;
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
 
-    const carPhotos = images.map((url,i) => {
-      if(i === 0) {
-        return <div key={i}> <img src={url.main} /> </div>
-      }
-      // if(i > 0 && displayAll) {
-      if(i > 0) {
-        return <div key={i}> <img src={url.images} /> </div>
-      }
-    })
-    
-    const settings = {
-      dots: false,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-    };
-
-    return(
-      <React.Fragment>
-        <Slider {...settings} className='photo-carousel'>
-          {carPhotos}
-        </Slider>
-      </React.Fragment>
-    );
-  }
-}
+  return (
+    <React.Fragment>
+      <Slider {...settings} className='photo-carousel'>
+        {carPhotos}
+      </Slider>
+    </React.Fragment>
+  );
+};
+  
 
 export default CarPhotos;
--- src/component/inventory/inventory-display.js
+++ src/component/inventory/inventory-display.js
@@ -28,34 +28,5 @@ const InventoryDisplay = (props) => {
   );
 };
 
-// class InventoryDisplay extends React.Component {
-//   constructor(props){
-//     super(props);
-//   }
-  
-//   render(){
-//     let {inventory, photos} = this.props;
-    
-//     return (
-//       <div className='car-list'>
-//         {Object.keys(inventory).map((car, i) => (
-//           <div 
-//             key={i} 
-//             className='car-item'
-//           >
-          
-//             <Car 
-//               car={inventory[car]} 
-//               folders={photos}
-//               displayFull={false} 
-
-//             />
-
-//           </div>
-//         ))}
-//       </div>
-//     );
-//   }
-// };
 
 export default InventoryDisplay;
